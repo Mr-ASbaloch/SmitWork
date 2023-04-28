41c31bd93a8f4a8d21ff181e726380cd349ca75b
abdul <abdulsaeed1356@gmail.com>
2023-04-28 10:45:40 +0500

    solved

---
+++ assigment -12/array-handling/1.js
@@ -0,0 +1,7 @@
+// Write a js program to read and print elements of array.
+
+let arr= [1,2,3,4,5,6,7,8];
+for (let index = 0; index < arr.length; index++) {
+ console.log(arr[index]);
+    
+};
---
+++ assigment -12/array-handling/2.js
@@ -0,0 +1,9 @@
+// . Write a js program to print all negative elements in an array.
+
+let arr=   [1,2,-3,4,5,-6]
+if (Number < 0) {
+    console.log(Number);
+}
+else{
+    console.log(arr);
+}
